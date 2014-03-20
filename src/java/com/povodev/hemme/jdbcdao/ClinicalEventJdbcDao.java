caef20fec4f873edc0446c8f7cba946bd25f53c8
Stefano <ssteee@gmail.com>
2014-03-20 14:30:22 +0100

    MySql mapping

--- src/java/com/povodev/hemme/jdbcdao/ClinicalEventJdbcDao.java
+++ src/java/com/povodev/hemme/jdbcdao/ClinicalEventJdbcDao.java
@@ -8,8 +8,14 @@ package com.povodev.hemme.jdbcdao;
 
 import com.povodev.hemme.bean.ClinicalEvent;
 import com.povodev.hemme.dao.ClinicalEventDao;
+import java.sql.Date;
 import java.util.ArrayList;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.simple.SimpleJdbcDaoSupport;
+import org.springframework.jdbc.core.simple.SimpleJdbcTemplate;
+import org.springframework.jdbc.core.support.JdbcDaoSupport;
 
 /**
  * JdbcDao implementation for ClinicalEvent
@@ -18,10 +24,7 @@ import org.springframework.jdbc.core.JdbcTemplate;
 public class ClinicalEventJdbcDao implements ClinicalEventDao{
     
     private JdbcTemplate jdbcTemplate;
-    public void setJdbcTemplate(JdbcTemplate jdbcTemplate){
-        this.jdbcTemplate = jdbcTemplate;
-    }
-
+    
     @Override
     public ClinicalEvent getClinicalEvent(int user_id, int clinicalEvent_id) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
