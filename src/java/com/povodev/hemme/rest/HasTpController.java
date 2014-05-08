41fa9f9d9e2fa50b68d21676d4bb23d42a658962
Stefano Munarini <ssteee@gmail.com>
2014-05-08 16:53:58 +0200

    Corrected ClinicalFolderMapper

--- src/java/com/povodev/hemme/rest/HasTpController.java
+++ src/java/com/povodev/hemme/rest/HasTpController.java
@@ -7,7 +7,9 @@
 package com.povodev.hemme.rest;
 
 import com.povodev.hemme.bean.HasTp;
+import com.povodev.hemme.bean.User;
 import com.povodev.hemme.jdbcdao.HasTpJdbcDao;
+import java.util.ArrayList;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -25,9 +27,9 @@ public class HasTpController {
     @Autowired
     private HasTpJdbcDao hasTpJdbcDao;
     
-    @RequestMapping("/patienList")
-    public @ResponseBody void newHasTp(
+    @RequestMapping("/patientList")
+    public @ResponseBody ArrayList<User> newHasTp(
             @RequestParam(value="tutor_id", required=true) int tutor_id){
-        hasTpJdbcDao.patientList(tutor_id);
+        return hasTpJdbcDao.patientList(tutor_id);
     }
 }
